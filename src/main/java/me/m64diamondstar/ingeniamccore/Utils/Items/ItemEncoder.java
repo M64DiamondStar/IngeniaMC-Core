ce265aa3a951c3c9bece6dac3580442f3a441407
BuildTools <unconfigured@null.spigotmc.org>
2022-09-24 18:59:14 +0200

    Created this class

---
+++ src/main/java/me/m64diamondstar/ingeniamccore/Utils/Items/ItemEncoder.java
@@ -0,0 +1,46 @@
+package me.m64diamondstar.ingeniamccore.Utils.Items;
+
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.util.io.BukkitObjectInputStream;
+import org.bukkit.util.io.BukkitObjectOutputStream;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Base64;
+
+public class ItemEncoder {
+
+    private ItemStack item;
+
+    public ItemEncoder(ItemStack item){
+        this.item = item;
+    }
+
+    public String encodedItem() {
+
+        String encodedItem = null;
+
+        try {
+
+            ByteArrayOutputStream io = new ByteArrayOutputStream();
+            BukkitObjectOutputStream os = new BukkitObjectOutputStream(io);
+
+            os.writeObject(item);
+
+            os.flush();
+
+            byte[] rawData = io.toByteArray();
+
+           encodedItem = Base64.getEncoder().encodeToString(rawData);
+
+            os.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return encodedItem;
+
+    }
+
+}
