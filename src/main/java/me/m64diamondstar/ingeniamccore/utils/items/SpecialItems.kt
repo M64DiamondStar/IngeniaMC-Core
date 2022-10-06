6e63c6370e17993db167e8810c80890745aa136f
BuildTools <unconfigured@null.spigotmc.org>
2022-10-06 22:10:03 +0200

    Added blank item

---
+++ src/main/java/me/m64diamondstar/ingeniamccore/utils/items/SpecialItems.kt
@@ -0,0 +1,19 @@
+package me.m64diamondstar.ingeniamccore.utils.items
+
+import me.m64diamondstar.ingeniamccore.utils.Colors
+import org.bukkit.Material
+import org.bukkit.inventory.ItemStack
+
+object SpecialItems {
+
+    val blankItem: ItemStack
+        get() {
+            val item = ItemStack(Material.FEATHER)
+            val meta = item.itemMeta
+            meta!!.setDisplayName(Colors.format("&0"))
+            meta.setCustomModelData(1)
+            item.itemMeta = meta
+            return item
+        }
+
+}
