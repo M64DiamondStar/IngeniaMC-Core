9ebedfd49a0a53e58d4f555a905ba2c929e04ab6
BuildTools <unconfigured@null.spigotmc.org>
2022-10-28 20:57:20 +0200

    EventPriority

--- src/main/java/me/m64diamondstar/ingeniamccore/utils/gui/GuiListener.kt
+++ src/main/java/me/m64diamondstar/ingeniamccore/utils/gui/GuiListener.kt
@@ -1,18 +1,19 @@
 package me.m64diamondstar.ingeniamccore.utils.gui
 
 import org.bukkit.event.EventHandler
+import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
 import org.bukkit.event.inventory.InventoryClickEvent
 
 class GuiListener : Listener {
 
-    @EventHandler
-    fun onInventoryClick(e: InventoryClickEvent){
-        val holder = e.inventory.holder
+    @EventHandler(priority = EventPriority.HIGH)
+    fun onInventoryClick(event: InventoryClickEvent){
+        val holder = event.inventory.holder
         if(holder is Gui) {
-            e.isCancelled = true
-            if (e.slot == -999) return
-            holder.handleInventory(e)
+            event.isCancelled = true
+            if (event.slot == -999) return
+            holder.handleInventory(event)
         }
     }
 }
