ec6c21686a6e2c46cb823353b5a3d9495e7b1b66
BuildTools <unconfigured@null.spigotmc.org>
2022-10-28 20:26:07 +0200

    Buttons

--- src/main/java/me/m64diamondstar/ingeniamccore/general/inventory/MainInventory.kt
+++ src/main/java/me/m64diamondstar/ingeniamccore/general/inventory/MainInventory.kt
@@ -1,11 +1,13 @@
 package me.m64diamondstar.ingeniamccore.general.inventory
 
+import me.m64diamondstar.ingeniamccore.cosmetics.inventory.CosmeticsInventory
 import me.m64diamondstar.ingeniamccore.general.player.IngeniaPlayer
 import me.m64diamondstar.ingeniamccore.utils.gui.Gui
 import me.m64diamondstar.ingeniamccore.utils.messages.Colors
 import me.m64diamondstar.ingeniamccore.utils.messages.MessageType
 import org.bukkit.Material
 import org.bukkit.event.inventory.InventoryClickEvent
+import org.bukkit.event.inventory.InventoryType
 import org.bukkit.inventory.ItemStack
 import org.bukkit.inventory.meta.ItemMeta
 import org.bukkit.inventory.meta.SkullMeta
@@ -17,6 +19,9 @@ class MainInventory(player: IngeniaPlayer): Gui(player) {
     private val achievementSlots = Array(4) {i -> i + 27}
     private val rankSlots = Array(4) {i -> i + 36}
     private val settingsSlots = Array(4) {i -> i + 45}
+    private val ridesSlots = Array(8) {i -> if(i in  0..3) i + 5 else i + 10 }
+    private val wardrobeSlots = Array(8) {i -> if(i in  0..3) i + 23 else i + 28 }
+    private val shopsSlots = Array(8) {i -> if(i in  0..3) i + 41 else i + 46 }
 
     override fun setDisplayName(): String {
         return Colors.format("&f\uF808手")
@@ -73,9 +78,37 @@ class MainInventory(player: IngeniaPlayer): Gui(player) {
         settingsSlots.forEach { inventory.setItem(it, transparentItem) }
 
 
+        transparentMeta.setDisplayName(Colors.format("#d4873b&lRides"))
+        transparentMeta.lore = listOf(Colors.format("${MessageType.LORE}Click to view the available rides."))
+        transparentItem.itemMeta = transparentMeta
+
+        ridesSlots.forEach { inventory.setItem(it, transparentItem) }
+
+
+        transparentMeta.setDisplayName(Colors.format("#c29b48&lWardrobe"))
+        transparentMeta.lore = listOf(Colors.format("${MessageType.LORE}Click to view your wardrobe."))
+        transparentItem.itemMeta = transparentMeta
+
+        wardrobeSlots.forEach { inventory.setItem(it, transparentItem) }
+
+
+        transparentMeta.setDisplayName(Colors.format("#e8d73c&lShops"))
+        transparentMeta.lore = listOf(Colors.format("${MessageType.LORE}Click to view the available park shops."))
+        transparentItem.itemMeta = transparentMeta
+
+        shopsSlots.forEach { inventory.setItem(it, transparentItem) }
+
+
     }
 
     override fun handleInventory(event: InventoryClickEvent) {
+
+        if(event.clickedInventory?.type == InventoryType.PLAYER) return
+
+        if(wardrobeSlots.contains(event.slot)){
+            val cosmeticsInventory = CosmeticsInventory(getPlayer().player, "國")
+            cosmeticsInventory.open()
+        }
 
     }
 
