5f610e02fff750521947032a9a0a7577b2f298ac
BuildTools <unconfigured@null.spigotmc.org>
2022-10-31 17:16:38 +0100

    Color from string

--- src/main/java/me/m64diamondstar/ingeniamccore/utils/messages/Colors.java
+++ src/main/java/me/m64diamondstar/ingeniamccore/utils/messages/Colors.java
@@ -1,7 +1,9 @@
 package me.m64diamondstar.ingeniamccore.utils.messages;
 
 import net.md_5.bungee.api.ChatColor;
+import org.bukkit.Bukkit;
 
+import java.awt.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -17,6 +19,24 @@ public class Colors {
             match = pattern.matcher(msg);
         }
         return ChatColor.translateAlternateColorCodes('&', msg).replace(":gs:", "✪");
+    }
+
+    public static Color getJavaColorFromString(String string){
+        String[] args = string.split(", ");
+
+        int r = 0;
+        int g = 0;
+        int b = 0;
+
+        try{
+            r = Integer.parseInt(args[0]);
+            g = Integer.parseInt(args[0]);
+            b = Integer.parseInt(args[0]);
+        }catch (NumberFormatException e){
+            Bukkit.getLogger().warning(e.getCause() + ": Error with converting String to Int in " + e.getClass());
+        }
+
+        return new Color(r, g, b);
     }
 
     public static String format(String msg, MessageType messageType) {
