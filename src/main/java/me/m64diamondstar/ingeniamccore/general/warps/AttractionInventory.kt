fb5a8ca9241eccc338fb950268cb3a4bbe75a8d3
BuildTools <unconfigured@null.spigotmc.org>
2022-11-04 22:31:08 +0100

    Back item

--- src/main/java/me/m64diamondstar/ingeniamccore/general/warps/AttractionInventory.kt
+++ src/main/java/me/m64diamondstar/ingeniamccore/general/warps/AttractionInventory.kt
@@ -2,10 +2,15 @@ package me.m64diamondstar.ingeniamccore.general.warps
 
 import me.m64diamondstar.ingeniamccore.attractions.utils.Attraction
 import me.m64diamondstar.ingeniamccore.attractions.utils.AttractionUtils
+import me.m64diamondstar.ingeniamccore.general.inventory.MainInventory
 import me.m64diamondstar.ingeniamccore.general.player.IngeniaPlayer
 import me.m64diamondstar.ingeniamccore.utils.gui.Gui
 import me.m64diamondstar.ingeniamccore.utils.messages.Colors
+import me.m64diamondstar.ingeniamccore.utils.messages.MessageType
+import org.bukkit.Material
 import org.bukkit.event.inventory.InventoryClickEvent
+import org.bukkit.inventory.ItemStack
+import org.bukkit.inventory.meta.ItemMeta
 
 class AttractionInventory(player: IngeniaPlayer): Gui(player) {
 
@@ -25,14 +30,30 @@ class AttractionInventory(player: IngeniaPlayer): Gui(player) {
                 getPlayer().player.teleport(slots[event.slot]?.getWarpLocation()!!)
             }
         }
+
+        if(event.slot == 49){
+            val mainInventory = MainInventory(getPlayer())
+            mainInventory.open()
+        }
     }
 
     override fun setInventoryItems() {
         for(attraction in AttractionUtils.getAllAttractions()){
             if(attraction.getWarpItem() != null && attraction.getWarpLocation() != null){
-                slots.put(inventory.firstEmpty(), attraction)
+                slots[inventory.firstEmpty()] = attraction
                 inventory.setItem(inventory.firstEmpty(), attraction.getWarpItem())
             }
         }
+
+        val transparentItem = ItemStack(Material.FEATHER)
+        val transparentMeta = transparentItem.itemMeta as ItemMeta
+        transparentMeta.setDisplayName(Colors.format(MessageType.ERROR + "&lGo Back"))
+        transparentMeta.lore = listOf(Colors.format("${MessageType.LORE}Click here to go back to the main menu."))
+        transparentMeta.setCustomModelData(1)
+
+        transparentItem.itemMeta = transparentMeta
+
+        inventory.setItem(49, transparentItem)
+
     }
 }
