056af44a4415b18953f423b561ecd2b755898319
BuildTools <unconfigured@null.spigotmc.org>
2022-11-07 21:15:51 +0100

    New SignActions

--- src/main/java/me/m64diamondstar/ingeniamccore/attractions/traincarts/SignRegistry.kt
+++ src/main/java/me/m64diamondstar/ingeniamccore/attractions/traincarts/SignRegistry.kt
@@ -2,15 +2,23 @@ package me.m64diamondstar.ingeniamccore.attractions.traincarts
 
 import com.bergerkiller.bukkit.tc.signactions.SignAction
 import me.m64diamondstar.ingeniamccore.attractions.traincarts.signs.SignActionAttraction
+import me.m64diamondstar.ingeniamccore.attractions.traincarts.signs.SignActionGiveExp
+import me.m64diamondstar.ingeniamccore.attractions.traincarts.signs.SignActionGiveGoldenStars
 
 object SignRegistry {
     private val signActionAttraction = SignActionAttraction()
+    private val signActionGiveExp = SignActionGiveExp()
+    private val signActionGiveGoldenStars = SignActionGiveGoldenStars()
 
     fun registerSigns(){
         SignAction.register(signActionAttraction)
+        SignAction.register(signActionGiveExp)
+        SignAction.register(signActionGiveGoldenStars)
     }
 
     fun unregisterSigns(){
         SignAction.unregister(signActionAttraction)
+        SignAction.unregister(signActionGiveExp)
+        SignAction.unregister(signActionGiveGoldenStars)
     }
 }
