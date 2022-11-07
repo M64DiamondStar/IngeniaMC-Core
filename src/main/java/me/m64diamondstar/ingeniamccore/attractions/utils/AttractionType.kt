9e0fc5fe3722bb6beea3a43d683e7b2e93d71559
BuildTools <unconfigured@null.spigotmc.org>
2022-11-07 21:14:46 +0100

    New format

--- src/main/java/me/m64diamondstar/ingeniamccore/attractions/utils/AttractionType.kt
+++ src/main/java/me/m64diamondstar/ingeniamccore/attractions/utils/AttractionType.kt
@@ -1,5 +1,10 @@
 package me.m64diamondstar.ingeniamccore.attractions.utils
 
 enum class AttractionType {
-    COASTER, FREEFALL, FRISBEE, TOPSPIN, SWINGSHIP
+    COASTER,
+    FREEFALL,
+    FRISBEE,
+    TOPSPIN,
+    SWINGSHIP,
+    PARKTRAIN
 }
