42a69dcc9a58ac60f4b37b15078ff8d80a88f7da
M64DiamondStar <59317305+M64DiamondStar@users.noreply.github.com>
2022-11-14 21:30:11 +0100

    Renamed main

--- src/main/java/me/m64diamondstar/ingeniamccore/general/listeners/ServerListPingListener.kt
+++ src/main/java/me/m64diamondstar/ingeniamccore/general/listeners/ServerListPingListener.kt
@@ -1,6 +1,6 @@
 package me.m64diamondstar.ingeniamccore.general.listeners
 
-import me.m64diamondstar.ingeniamccore.Main
+import me.m64diamondstar.ingeniamccore.IngeniaMC
 import me.m64diamondstar.ingeniamccore.utils.messages.Colors
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
@@ -10,7 +10,7 @@ class ServerListPingListener: Listener {
 
     @EventHandler
     fun onServerListPing(event: ServerListPingEvent){
-        event.motd = Colors.format(Main.plugin.config.getString("Motd")!!)
+        event.motd = Colors.format(IngeniaMC.plugin.config.getString("Motd")!!)
     }
 
 }
