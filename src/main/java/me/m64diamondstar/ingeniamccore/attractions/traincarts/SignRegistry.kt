cac4d3007002db4b8e2ee968854a88b01cb0e4e0
M64DiamondStar <59317305+M64DiamondStar@users.noreply.github.com>
2022-11-14 21:29:05 +0100

    Added SignActionShow.kt

--- src/main/java/me/m64diamondstar/ingeniamccore/attractions/traincarts/SignRegistry.kt
+++ src/main/java/me/m64diamondstar/ingeniamccore/attractions/traincarts/SignRegistry.kt
@@ -4,21 +4,25 @@ import com.bergerkiller.bukkit.tc.signactions.SignAction
 import me.m64diamondstar.ingeniamccore.attractions.traincarts.signs.SignActionAttraction
 import me.m64diamondstar.ingeniamccore.attractions.traincarts.signs.SignActionGiveExp
 import me.m64diamondstar.ingeniamccore.attractions.traincarts.signs.SignActionGiveGoldenStars
+import me.m64diamondstar.ingeniamccore.attractions.traincarts.signs.SignActionShow
 
 object SignRegistry {
     private val signActionAttraction = SignActionAttraction()
     private val signActionGiveExp = SignActionGiveExp()
     private val signActionGiveGoldenStars = SignActionGiveGoldenStars()
+    private val signActionShow = SignActionShow()
 
     fun registerSigns(){
         SignAction.register(signActionAttraction)
         SignAction.register(signActionGiveExp)
         SignAction.register(signActionGiveGoldenStars)
+        SignAction.register(signActionShow)
     }
 
     fun unregisterSigns(){
         SignAction.unregister(signActionAttraction)
         SignAction.unregister(signActionGiveExp)
         SignAction.unregister(signActionGiveGoldenStars)
+        SignAction.unregister(signActionShow)
     }
 }
