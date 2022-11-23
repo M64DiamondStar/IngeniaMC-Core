311b82046a40891680fb874ed0ae6a3318209829
M64DiamondStar <59317305+M64DiamondStar@users.noreply.github.com>
2022-11-23 21:57:18 +0100

    Added spawn and despawn functions

--- src/main/java/me/m64diamondstar/ingeniamccore/attractions/custom/Coaster.kt
+++ src/main/java/me/m64diamondstar/ingeniamccore/attractions/custom/Coaster.kt
@@ -34,16 +34,71 @@ class Coaster(category: String, name: String): Attraction(category, name) {
         return list
     }
 
+    /**
+     * Sets the station sign of a specific row in this attraction
+     */
     fun setRowStation(row: Int, location: Location){
         this.getConfig().set("Data.Station.Rows.$row.StationSign", "${location.x}, ${location.y}, ${location.z}")
         this.reloadConfig()
     }
 
+    /**
+     * Get the location of the station sign of a specific row
+     * @return the location where a redstone signal should be given
+     */
     private fun getRowStation(row: Int): Location? {
         if(this.getConfig().get("Data.Station.Rows.$row.StationSign") == null)
             return null
 
         val args = this.getConfig().getString("Data.Station.Rows.$row.StationSign")?.split(", ")
+        val x = args?.get(0)?.toDouble()!!
+        val y = args[1].toDouble()
+        val z = args[2].toDouble()
+
+        return Location(getWorld(), x, y, z)
+    }
+
+    /**
+     * Sets the spawn sign of a specific row in this attraction
+     */
+    fun setRowSpawn(row: Int, location: Location){
+        this.getConfig().set("Data.Station.Rows.$row.SpawnSign", "${location.x}, ${location.y}, ${location.z}")
+        this.reloadConfig()
+    }
+
+    /**
+     * Get the location of the spawn sign of a specific row
+     * @return the location where a redstone signal should be given
+     */
+    private fun getRowSpawn(row: Int): Location? {
+        if(this.getConfig().get("Data.Station.Rows.$row.SpawnSign") == null)
+            return null
+
+        val args = this.getConfig().getString("Data.Station.Rows.$row.SpawnSign")?.split(", ")
+        val x = args?.get(0)?.toDouble()!!
+        val y = args[1].toDouble()
+        val z = args[2].toDouble()
+
+        return Location(getWorld(), x, y, z)
+    }
+
+    /**
+     * Sets the despawn sign of a specific row in this attraction
+     */
+    fun setRowDespawn(row: Int, location: Location){
+        this.getConfig().set("Data.Station.Rows.$row.DespawnSign", "${location.x}, ${location.y}, ${location.z}")
+        this.reloadConfig()
+    }
+
+    /**
+     * Get the location of the despawn sign of a specific row
+     * @return the location where a redstone signal should be given
+     */
+    private fun getRowDespawn(row: Int): Location? {
+        if(this.getConfig().get("Data.Station.Rows.$row.DespawnSign") == null)
+            return null
+
+        val args = this.getConfig().getString("Data.Station.Rows.$row.DespawnSign")?.split(", ")
         val x = args?.get(0)?.toDouble()!!
         val y = args[1].toDouble()
         val z = args[2].toDouble()
@@ -75,6 +130,40 @@ class Coaster(category: String, name: String): Attraction(category, name) {
         Bukkit.getScheduler().scheduleSyncDelayedTask(IngeniaMC.plugin, {
             getRowStation(1)!!.block.type = Material.AIR
         }, 5L)
+    }
+
+    fun spawn(){
+        for(row in getRows()) {
+
+            if (getRowSpawn(row) == null) {
+                Bukkit.getLogger()
+                    .warning("Spawn sign(s) for ${getName()} in ${getCategory()} still has to be made! Please do this as quickly as possible!")
+                return
+            }
+            getRowSpawn(row)!!.block.type = Material.REDSTONE_TORCH
+
+            Bukkit.getScheduler().scheduleSyncDelayedTask(IngeniaMC.plugin, {
+                getRowSpawn(row)!!.block.type = Material.AIR
+            }, 5L)
+
+        }
+    }
+
+    fun despawn(){
+        for(row in getRows()) {
+
+            if (getRowDespawn(row) == null) {
+                Bukkit.getLogger()
+                    .warning("Depawn sign(s) for ${getName()} in ${getCategory()} still has to be made! Please do this as quickly as possible!")
+                return
+            }
+            getRowDespawn(row)!!.block.type = Material.REDSTONE_TORCH
+
+            Bukkit.getScheduler().scheduleSyncDelayedTask(IngeniaMC.plugin, {
+                getRowDespawn(row)!!.block.type = Material.AIR
+            }, 5L)
+
+        }
     }
 
 }
