74cca3e715d5b8ddecef3896737ac2e8bbe292e6
M64DiamondStar <59317305+M64DiamondStar@users.noreply.github.com>
2022-12-30 21:26:31 +0100

    Update because previous system was stupid

--- src/main/java/me/m64diamondstar/ingeniamccore/utils/rewards/RewardType.kt
+++ src/main/java/me/m64diamondstar/ingeniamccore/utils/rewards/RewardType.kt
@@ -1,9 +1,25 @@
 package me.m64diamondstar.ingeniamccore.utils.rewards
 
+import me.m64diamondstar.ingeniamccore.general.player.IngeniaPlayer
+
 enum class RewardType {
 
-    GS,
-    WAND,
-    EXP
+    EXP {
+        override fun execute(player: IngeniaPlayer, arg: String) {
+            player.addExp(arg.toLong())
+        }
+    },
+    GS {
+        override fun execute(player: IngeniaPlayer, arg: String) {
+            player.addBal(arg.toLong())
+        }
+    },
+    WAND {
+        override fun execute(player: IngeniaPlayer, arg: String) {
+            player.givePermission("ingeniawands.$arg")
+        }
+    };
+
+    abstract fun execute(player: IngeniaPlayer, arg: String)
 
 }
