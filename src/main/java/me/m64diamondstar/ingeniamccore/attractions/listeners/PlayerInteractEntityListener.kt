03ecfc5bc0317f6f6ec088c96c6cf9de69cd519e
M64DiamondStar <59317305+M64DiamondStar@users.noreply.github.com>
2022-12-30 21:27:15 +0100

    Locked entity check

--- src/main/java/me/m64diamondstar/ingeniamccore/attractions/listeners/PlayerInteractEntityListener.kt
+++ src/main/java/me/m64diamondstar/ingeniamccore/attractions/listeners/PlayerInteractEntityListener.kt
@@ -3,6 +3,7 @@ package me.m64diamondstar.ingeniamccore.attractions.listeners
 import me.m64diamondstar.ingeniamccore.attractions.custom.Coaster
 import me.m64diamondstar.ingeniamccore.attractions.custom.FreeFall
 import me.m64diamondstar.ingeniamccore.attractions.custom.Slide
+import me.m64diamondstar.ingeniamccore.utils.entities.EntityUtils
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerInteractAtEntityEvent
@@ -12,12 +13,20 @@ class PlayerInteractEntityListener: Listener {
 
     @EventHandler
     fun onEntityClick(event: PlayerInteractEntityEvent){
+
+        val entity = event.rightClicked
+        if(EntityUtils.isLocked(entity)) return
+
         Coaster.clickEvent(event.rightClicked)
         Slide.clickEvent(event.rightClicked)
     }
 
     @EventHandler
     fun onAtEntityClick(event: PlayerInteractAtEntityEvent){
+
+        val entity = event.rightClicked
+        if(EntityUtils.isLocked(entity)) return
+
         FreeFall.clickEvent(event)
     }
 }
