69507190a920fba60358cfb4bfa5f6dd557dbc09
M64DiamondStar <59317305+M64DiamondStar@users.noreply.github.com>
2022-12-30 21:29:00 +0100

    Added random present generator

--- src/main/java/me/m64diamondstar/ingeniamccore/utils/items/Items.kt
+++ src/main/java/me/m64diamondstar/ingeniamccore/utils/items/Items.kt
@@ -8,6 +8,7 @@ import org.bukkit.inventory.meta.SkullMeta
 import java.lang.reflect.Field
 import java.util.*
 
+
 object Items {
 
     fun getPlayerHead(fUrl: String?): ItemStack {
@@ -38,5 +39,25 @@ object Items {
         }
         skull.itemMeta = skullMeta
         return skull
+    }
+
+    fun getRandomPresentProfile(): GameProfile{
+        val randomTexture = listOf("1c6274c22d726fc120ce25736030cc8af238b44bcbf56655207953c414422f",
+            "6c8652bfdb7adde128e7eacc50d16eb9f487a3209b304de3b9697cebf13323b",
+            "4acb3c1e1b34f8734aedfabd1e1f5e0b280bef924fb8bbf3e692d2538266f4",
+            "928e692d86e224497915a39583dbe38edffd39cbba457cc95a7ac3ea25d445",
+            "6cef9aa14e884773eac134a4ee8972063f466de678363cf7b1a21a85b7",
+            "ed97f4f44e796f79ca43097faa7b4fe91c445c76e5c26a5ad794f5e479837",).shuffled().first()
+
+        val profile = GameProfile(UUID.randomUUID(), null)
+        val encodedData: ByteArray = Base64.getEncoder().encode(
+            java.lang.String.format(
+                "{textures:{SKIN:{url:\"%s\"}}}",
+                "http://textures.minecraft.net/texture/$randomTexture"
+            ).toByteArray()
+        )
+
+        profile.properties.put("textures", Property("textures", String(encodedData)))
+        return profile
     }
 }
