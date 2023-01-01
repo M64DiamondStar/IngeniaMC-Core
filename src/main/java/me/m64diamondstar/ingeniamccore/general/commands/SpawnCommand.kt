b408d7de2504c072793b57856964c577cabca5ca
M64DiamondStar <59317305+M64DiamondStar@users.noreply.github.com>
2023-01-01 17:55:20 +0100

    Created

---
+++ src/main/java/me/m64diamondstar/ingeniamccore/general/commands/SpawnCommand.kt
@@ -0,0 +1,24 @@
+package me.m64diamondstar.ingeniamccore.general.commands
+
+import me.m64diamondstar.ingeniamccore.utils.messages.Messages
+import org.bukkit.Location
+import org.bukkit.command.Command
+import org.bukkit.command.CommandExecutor
+import org.bukkit.command.CommandSender
+import org.bukkit.entity.Player
+
+class SpawnCommand: CommandExecutor {
+
+    override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>?): Boolean {
+
+        if (sender !is Player) {
+            sender.sendMessage(Messages.noPlayer())
+            return false
+        }
+
+        sender.teleport(Location(sender.world, 0.5, 52.0, 0.5, 0F, 0F))
+
+        return false
+    }
+
+}
