72e7ed1cd4db8788931d13e5407261975484ba29
M64DiamondStar <59317305+M64DiamondStar@users.noreply.github.com>
2023-05-18 16:05:54 +0200

    Added IP Command and added permissions

--- src/main/java/me/m64diamondstar/ingeniamccore/discord/listeners/GuildReadyListener.kt
+++ src/main/java/me/m64diamondstar/ingeniamccore/discord/listeners/GuildReadyListener.kt
@@ -1,7 +1,9 @@
 package me.m64diamondstar.ingeniamccore.discord.listeners
 
+import net.dv8tion.jda.api.Permission
 import net.dv8tion.jda.api.events.guild.GuildReadyEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
+import net.dv8tion.jda.api.interactions.commands.DefaultMemberPermissions
 import net.dv8tion.jda.api.interactions.commands.OptionType
 import net.dv8tion.jda.api.interactions.commands.build.Commands
 
@@ -14,22 +16,27 @@ class GuildReadyListener: ListenerAdapter() {
 
             .addCommands(Commands.slash("help", "Opens the help menu."))
 
-            .addCommands(Commands.slash("ticket", "Edit ticket configuration."))
-
             .addCommands(Commands.slash("close", "Closes a ticket."))
 
             .addCommands(Commands.slash("meme", "Show a random meme."))
 
             .addCommands(Commands.slash("whatismyid", "Get your discord ID."))
 
+            .addCommands(Commands.slash("ip", "Retrieve the server IP and version."))
+
             .addCommands(Commands.slash("link", "Link your Minecraft account with your Discord account.")
                 .addOption(OptionType.STRING, "name", "Your in-game Minecraft username.", true))
+
+            // ADMIN-ONLY COMMANDS
+            .addCommands(Commands.slash("ticket", "Edit ticket configuration.")
+                .setDefaultPermissions(DefaultMemberPermissions.enabledFor(Permission.ADMINISTRATOR)))
 
             .addCommands(Commands.slash("embed", "Create an embedded message.")
                 .addOption(OptionType.STRING, "title", "The title.", true)
                 .addOption(OptionType.STRING, "description", "The description.", true)
                 .addOption(OptionType.STRING, "color", "The color.", false)
-                .addOption(OptionType.CHANNEL, "channel", "The channel to send the message in", false))
+                .addOption(OptionType.CHANNEL, "channel", "The channel to send the message in", false)
+                .setDefaultPermissions(DefaultMemberPermissions.enabledFor(Permission.ADMINISTRATOR)))
             .queue()
     }
 
