4bf8be42460ad4ff835960e6d71d04c547ad521b
M64DiamondStar <59317305+M64DiamondStar@users.noreply.github.com>
2023-05-18 16:05:37 +0200

    Added IP Command

--- src/main/java/me/m64diamondstar/ingeniamccore/discord/bot/DiscordBot.kt
+++ src/main/java/me/m64diamondstar/ingeniamccore/discord/bot/DiscordBot.kt
@@ -7,6 +7,7 @@ import me.m64diamondstar.ingeniamccore.discord.commands.linking.LinkCommand
 import me.m64diamondstar.ingeniamccore.discord.commands.profile.ProfileCommand
 import me.m64diamondstar.ingeniamccore.discord.commands.tickets.CloseCommand
 import me.m64diamondstar.ingeniamccore.discord.commands.tickets.TicketCommand
+import me.m64diamondstar.ingeniamccore.discord.commands.utils.IpCommand
 import me.m64diamondstar.ingeniamccore.discord.commands.utils.WhatismyidCommand
 import me.m64diamondstar.ingeniamccore.discord.listeners.GuildReadyListener
 import net.dv8tion.jda.api.JDA
@@ -22,7 +23,7 @@ object DiscordBot {
         jda = JDABuilder.createDefault(IngeniaMC.plugin.config.getString("Discord.Bot.Token"))
             .enableIntents(GatewayIntent.GUILD_MEMBERS, GatewayIntent.DIRECT_MESSAGES)
             .addEventListeners(GuildReadyListener(), MemeCommand(), EmbedCommand(), TicketCommand(),
-                CloseCommand(), WhatismyidCommand(), LinkCommand(), ProfileCommand()
+                CloseCommand(), WhatismyidCommand(), LinkCommand(), ProfileCommand(), IpCommand()
             )
             .setActivity(Activity.playing("play.IngeniaMC.net on 1.19.x"))
             .build()
