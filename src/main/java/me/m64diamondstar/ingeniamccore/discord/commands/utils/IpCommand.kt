1aeffe2c385df6725f0f3d9f9135e930fd1d0598
M64DiamondStar <59317305+M64DiamondStar@users.noreply.github.com>
2023-05-18 16:04:56 +0200

    Initial commit

---
+++ src/main/java/me/m64diamondstar/ingeniamccore/discord/commands/utils/IpCommand.kt
@@ -0,0 +1,27 @@
+package me.m64diamondstar.ingeniamccore.discord.commands.utils
+
+import net.dv8tion.jda.api.EmbedBuilder
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
+import net.dv8tion.jda.api.hooks.ListenerAdapter
+import java.awt.Color
+
+class IpCommand: ListenerAdapter() {
+
+    override fun onSlashCommandInteraction(event: SlashCommandInteractionEvent) {
+        if(!event.name.equals("ip", ignoreCase = true))
+            return
+
+        event.deferReply(true).queue()
+
+        val embedBuilder = EmbedBuilder()
+
+        embedBuilder.setTitle("IP")
+        embedBuilder.setDescription("IP: `play.IngeniaMC.net`\n" +
+                "Version: `1.19 - 1.19.2`")
+        embedBuilder.setColor(Color.decode("#ffb833"))
+
+        event.hook.sendMessageEmbeds(embedBuilder.build()).queue()
+
+    }
+
+}
