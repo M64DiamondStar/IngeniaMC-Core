0d867b2c8398b2bb0e74448c4d5c0d2853c772ac
M64DiamondStar <59317305+M64DiamondStar@users.noreply.github.com>
2024-05-09 22:49:40 +0200

    Updated OpenAudioMc

--- src/main/java/me/m64diamondstar/ingeniamccore/general/areas/listeners/AudioConnectListener.kt
+++ src/main/java/me/m64diamondstar/ingeniamccore/general/areas/listeners/AudioConnectListener.kt
@@ -1,7 +1,7 @@
 package me.m64diamondstar.ingeniamccore.general.areas.listeners
 
-import com.craftmend.openaudiomc.api.impl.event.events.ClientConnectEvent
-import me.m64diamondstar.ingeniamccore.IngeniaMC
+import com.craftmend.openaudiomc.api.EventApi
+import com.craftmend.openaudiomc.api.events.client.ClientConnectEvent
 import me.m64diamondstar.ingeniamccore.utils.messages.MessageType
 import net.kyori.adventure.audience.Audience
 import net.kyori.adventure.text.minimessage.MiniMessage
@@ -10,15 +10,15 @@ import org.bukkit.Bukkit
 object AudioConnectListener {
 
     fun startListeners(){
-        IngeniaMC.audioApi.eventDriver
-            .on(ClientConnectEvent::class.java)
-            .setHandler {
-                val player = Bukkit.getPlayer(it.client.owner.uniqueId) ?: return@setHandler
-                (player as Audience).sendMessage(
-                    MiniMessage.miniMessage().deserialize(
-                        "<${MessageType.ERROR}>Please use /fixaudio if your audio isn't playing properly.")
-                )
-            }
+
+        EventApi.getInstance().registerHandler(ClientConnectEvent::class.java) {
+            val player = Bukkit.getPlayer(it.client.actor.uniqueId) ?: return@registerHandler
+            (player as Audience).sendMessage(
+                MiniMessage.miniMessage().deserialize(
+                    "<${MessageType.ERROR}>Please use /fixaudio if your audio isn't playing properly.")
+            )
+        }
+
     }
 
 }
