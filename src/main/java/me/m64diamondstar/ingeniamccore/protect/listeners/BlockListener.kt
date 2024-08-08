0843573927f54c794509b731f9915bb8dd6a414d
M64DiamondStar <59317305+M64DiamondStar@users.noreply.github.com>
2024-08-08 11:11:37 +0200

    Cancel decorated pot breaks

--- src/main/java/me/m64diamondstar/ingeniamccore/protect/listeners/BlockListener.kt
+++ src/main/java/me/m64diamondstar/ingeniamccore/protect/listeners/BlockListener.kt
@@ -1,9 +1,11 @@
 package me.m64diamondstar.ingeniamccore.protect.listeners
 
+import org.bukkit.Material
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.block.BlockBreakEvent
 import org.bukkit.event.block.BlockPlaceEvent
+import org.bukkit.event.entity.ProjectileHitEvent
 
 class BlockListener: Listener {
 
@@ -19,6 +21,12 @@ class BlockListener: Listener {
         val player = event.player
         if(player.hasPermission("ingenia.admin") || player.isOp) return
         event.isCancelled = true
+    }
+
+    @EventHandler
+    fun onProjectileHit(event: ProjectileHitEvent){
+        if(event.hitBlock == null) return
+        if(event.hitBlock!!.type == Material.DECORATED_POT) event.isCancelled = true
     }
 
 }
