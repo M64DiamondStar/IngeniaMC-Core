e639ff32edda565e726fb95c2f800068053af8e3
M64DiamondStar <59317305+M64DiamondStar@users.noreply.github.com>
2024-09-05 17:39:35 +0200

    Urls now click correctly

--- src/main/java/me/m64diamondstar/ingeniamccore/general/commands/MediaCommands.kt
+++ src/main/java/me/m64diamondstar/ingeniamccore/general/commands/MediaCommands.kt
@@ -12,22 +12,22 @@ class MediaCommands: CommandExecutor {
 
         when(command.name){
             "website" -> (sender as Audience).sendMessage(MiniMessage.miniMessage()
-                .deserialize("<${MessageType.INGENIA}><click:open_url:https://ingeniamc.net>Click to visit our <#ffffff>website!"))
+                .deserialize("<${MessageType.INGENIA}><click:open_url:\"https://ingeniamc.net\">Click to visit our <#ffffff>website!"))
 
             "store" -> (sender as Audience).sendMessage(MiniMessage.miniMessage()
-                .deserialize("<${MessageType.INGENIA}><click:open_url:https://store.ingeniamc.net/>Click to visit our <#ffffff>store!"))
+                .deserialize("<${MessageType.INGENIA}><click:open_url:\"https://store.ingeniamc.net\">Click to visit our <#ffffff>store!"))
 
             "discord" -> (sender as Audience).sendMessage(MiniMessage.miniMessage()
-                .deserialize("<#7289DA><click:open_url:https://discord.com/invite/qv2vsAsDs8/>Click to join our <#ffffff>discord server!"))
+                .deserialize("<#7289DA><click:open_url:\"https://discord.com/invite/qv2vsAsDs8\">Click to join our <#ffffff>discord server!"))
 
             "youtube" -> (sender as Audience).sendMessage(MiniMessage.miniMessage()
-                .deserialize("<#CD201F><click:open_url:https://www.youtube.com/@Ingenia_MC>Click to check out our <#ffffff>youtube channel!"))
+                .deserialize("<#CD201F><click:open_url:\"https://www.youtube.com/@Ingenia_MC\">Click to check out our <#ffffff>youtube channel!"))
 
             "tiktok" -> (sender as Audience).sendMessage(MiniMessage.miniMessage()
-                .deserialize("<gradient:#ff0050:#00f2ea><click:open_url:https://www.tiktok.com/@ingeniamc>Click to view our <#ffffff>tiktok!"))
+                .deserialize("<gradient:#ff0050:#00f2ea><click:open_url:\"https://www.tiktok.com/@ingeniamc\">Click to view our <#ffffff>tiktok!"))
 
             "instagram" -> (sender as Audience).sendMessage(MiniMessage.miniMessage()
-                .deserialize("<#8a3ab9><click:open_url:https://www.instagram.com/ingenia_mc/>Click to open our <#ffffff>insta!"))
+                .deserialize("<#8a3ab9><click:open_url:\"https://www.instagram.com/ingenia_mc\">Click to open our <#ffffff>insta!"))
         }
 
         return true
